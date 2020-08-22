f6b183e077fb3cf01fab90b302fe3b23cea37dc1
jamvo <j.stojicik@gmail.com>
2020-08-22 16:14:42 +0200

    Adding small jokes app

---
+++ src/main/java/jovica/springframework/joke/jokeapp/services/JokeService.java
@@ -0,0 +1,7 @@
+package jovica.springframework.joke.jokeapp.services;
+
+import org.springframework.stereotype.Service;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/jovica/springframework/joke/jokeapp/services/jokeServiceImpl.java
@@ -0,0 +1,18 @@
+package jovica.springframework.joke.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class jokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public jokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
